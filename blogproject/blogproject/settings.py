a17c55f58335a78133f23fba3e0e15a1b82facb4
gqp <446105468@qq.com>
2022-04-17 22:31:21 +0800

    update

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '6dfnxdw=u@_&pc(jt8=qe^xuixlbpqu292z(^o#0av!88_z+4c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
